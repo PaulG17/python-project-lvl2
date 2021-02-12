336d2ee586e743ca3ea47792a436d5e74164bceb
Paul Kozlov <gg1779@gmail.com>
2021-02-12 22:58:49 +0300

    correct lint

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,21 +1,21 @@
 import json
+
+
 def main():
     first_data = json.load(open('example1.json'))
     second_data = json.load(open('example2.json'))
     first_dict_keys = first_data.keys()
     second_dict_keys = second_data.keys()
     result = []
-    for k in second_dict_keys - first_dict_keys: #добавлен
+    for k in second_dict_keys - first_dict_keys:  # добавлен
         result.append(f"+ {k}: {second_data[k]}")
-    for k in first_dict_keys - second_dict_keys: #удален
+    for k in first_dict_keys - second_dict_keys:  # удален
         result.append(f"- {k}: {first_data[k]}")
     for k in first_dict_keys & second_dict_keys:
         if first_data[k] == second_data[k]:
-            result.append(f"  {k}: {first_data[k]}") #равен
+            result.append(f"  {k}: {first_data[k]}")  # равен
         if first_data[k] != second_data[k]:
-            result.append(f"- {k}: {first_data[k]}") #не равен
-            result.append(f"+ {k}: {second_data[k]}") #не равен
+            result.append(f"- {k}: {first_data[k]}")  # не равен
+            result.append(f"+ {k}: {second_data[k]}")  # не равен
     final = sorted(result)
     print("\n".join(final))
-
-
